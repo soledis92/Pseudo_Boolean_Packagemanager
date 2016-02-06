b6055a6e451a75f5b2cf42be7acecaf6052ddb65
soledis92 <soledis92@gmail.com>
2016-02-06 14:27:38 +0100

    Add new test-scenario 'addieren'.

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,14 @@
 #include "gtest/gtest.h"
+#include "addieren.h"
 
 TEST(mocktest, mocking)
 {
   EXPECT_EQ(1, 1);
 }
 
+TEST(add,positive)
+{
+    Addieren addClassIntance = Addieren();
+    EXPECT_EQ(5, addClassIntance.add(3,2));
+}
 
